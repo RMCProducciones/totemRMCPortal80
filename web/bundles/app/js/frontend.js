5bd6c2e2e1fcfb9e40b00d48839d42cd4a855dae
Martinez2348 <cami2348@hotmail.com>
2016-05-04 14:41:35 -0500

    video

--- web/bundles/app/js/frontend.js
+++ web/bundles/app/js/frontend.js
@@ -1,6 +1,6 @@
 url_app = document.getElementById("path").value;
 
-url_video = "http://localhost:8080/totemRMCPortal80/web";
+url_video = "http://127.0.0.1/totemRMCPortal80/web";
 //url_video = "http://127.0.0.1/RMC/totemRMCPortal80/web";
 
 //document.oncontextmenu = function(){return false}
